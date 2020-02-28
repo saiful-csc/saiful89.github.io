26c238cac76a17f544f7183e806661546febc48f
saiful <58758203+saiful-csc@users.noreply.github.com>
2020-02-27 20:25:06 -0500

    Update App.js
    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/saiful-csc"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          check me out!
         </a>
       </header>
     </div>
