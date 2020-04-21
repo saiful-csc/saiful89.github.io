5879183f9ff6f075e800e45c564f20fbc9536c96
saiful isalm <saiful_topu89@yahoo.com>
2020-04-20 22:52:52 -0400

    aboutme

---
+++ src/components/aboutme.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return(
+      <div> <h1 style={{ width: '20%', margin: 'auto', paddingTop: '0em'}}>About Me</h1>
+      <h3 style={{ width: '90%', margin: 'auto', paddingTop: '2em'}}>
+      Hey, my name is Saiful islam. I am a web developer. 
+      I like to work as a front-end web developer which users see 
+      and interact with in a web application </h3> </div>
+      
+      
+    )
+  }
+}
+
+export default About;
